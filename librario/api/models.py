94f49eaa33eb26423002311f8a1fac45ac09e995
josevenzke <josevenzkejr@gmail.com>
2021-07-11 20:48:05 -0300

    create book model

--- librario/api/models.py
+++ librario/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=300)
+    author = models.CharField(max_length=300)
+    pages = models.IntegerField()
+    class Meta:
+        db_table = 'books'
