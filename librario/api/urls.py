3590bb1ad1140f1d7254c44a36101d0539383a62
josevenzke <josevenzkejr@gmail.com>
2021-07-12 18:51:48 -0300

    created add book api endpoint

--- librario/api/urls.py
+++ librario/api/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('book-list/', views.listBook),
+    path('list-book/', views.listBook),
+    path('add-book/',views.addBook)
 ]
