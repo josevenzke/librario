e2064035de3aa1b92309acceca5c1e2ef5ca4a51
josevenzke <josevenzkejr@gmail.com>
2021-07-12 18:52:53 -0300

    added connection to postgre database

--- librario/librario/settings.py
+++ librario/librario/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'librario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'librario',
+        'USER': 'jose',
+        'PASSWORD': '1415926',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
