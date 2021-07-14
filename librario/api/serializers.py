031aed93c1f373810e304e12d4f5346444876cf9
josevenzke <josevenzkejr@gmail.com>
2021-07-13 23:01:16 -0300

    created serializer for review model

--- librario/api/serializers.py
+++ librario/api/serializers.py
@@ -1,7 +1,13 @@
+from django.db.models import fields
 from rest_framework import serializers
-from .models import Book
+from .models import Book, Review
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'author', 'pages']
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
