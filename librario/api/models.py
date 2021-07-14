f6a5bea77b61c5ad4b484565bd8db6b7ddc0f040
josevenzke <josevenzkejr@gmail.com>
2021-07-13 21:36:57 -0300

    created new review model

--- librario/api/models.py
+++ librario/api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 class Book(models.Model):
@@ -7,3 +9,12 @@ class Book(models.Model):
     pages = models.IntegerField()
     class Meta:
         db_table = 'books'
+
+class Review(models.Model):
+    stars = models.IntegerField()
+    description = models.CharField(max_length=1000)
+    recomends = models.BooleanField()
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    book_id = ForeignKey(Book,on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'review'
