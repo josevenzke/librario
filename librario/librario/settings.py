b732404c339b115750af10f3a0a6b2ad07452a90
josevenzke <josevenzkejr@gmail.com>
2021-09-02 00:22:58 -0300

    added rest framework configuration

--- librario/librario/settings.py
+++ librario/librario/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
 
@@ -71,6 +72,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'librario.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
