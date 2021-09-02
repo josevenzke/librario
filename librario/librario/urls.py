c01d0d6d2fae0379b16fd353f2702224705caf5b
josevenzke <josevenzkejr@gmail.com>
2021-09-02 00:23:23 -0300

    added url for authentication

--- librario/librario/urls.py
+++ librario/librario/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('api-token/', views.obtain_auth_token, name="api-token")
 ]
