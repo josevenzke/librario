5583b500e43a72bd4d539812509eb3fd908487cb
josevenzke <josevenzkejr@gmail.com>
2021-09-07 17:50:33 -0300

    now using env constants

--- librario/librario/settings.py
+++ librario/librario/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-khdwgju97gf$ztt&jhq2$5bka#hdorq%jxngxfr+-7_79lgu3_'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,9 +92,9 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'wlfthsmz',
-        'USER': 'wlfthsmz',
-        'PASSWORD': '1CdwpO7X7X6brNblvksj4fdLeAWO5nP3',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
         'HOST': 'motty.db.elephantsql.com',
     }
 }
