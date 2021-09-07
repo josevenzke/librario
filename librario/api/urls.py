c49b05091fed14e6ce423815e1f582795ebd7239
josevenzke <josevenzkejr@gmail.com>
2021-09-06 21:46:05 -0300

    better url names

--- librario/api/urls.py
+++ librario/api/urls.py
@@ -2,8 +2,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('list-book/', views.listBook,name='list-book'),
+    path('list-books/', views.listBooks,name='list-books'),
     path('add-book/',views.addBook,name='add-book'),
-    path('list-review/<int:book_id>/',views.listReviewsFromBook,name='list-review'),
+    path('list-book-reviews/<int:book_id>/',views.listReviewsFromBook,name='list-book-reviews'),
     path('add-review/<int:book_id>/',views.addReview,name='add-review')
 ]
