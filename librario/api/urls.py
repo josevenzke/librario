016c8a6d2090c7a9d57e75765f3d81b44139553b
josevenzke <josevenzkejr@gmail.com>
2021-09-06 21:42:35 -0300

    created endpoint for review list

--- librario/api/urls.py
+++ librario/api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('list-book/', views.listBook,name='list-book'),
     path('add-book/',views.addBook,name='add-book'),
+    path('list-review/<int:book_id>/',views.listReviewsFromBook,name='list-review'),
     path('add-review/<int:book_id>/',views.addReview,name='add-review')
 ]
