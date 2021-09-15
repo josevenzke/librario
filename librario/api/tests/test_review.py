e2578b37cab3fd98409593c7020a44cb1595435b
josevenzke <josevenzkejr@gmail.com>
2021-09-15 00:38:01 -0300

    now correctly creating objects before assigning them

--- librario/api/tests/test_review.py
+++ librario/api/tests/test_review.py
@@ -5,13 +5,15 @@ class BookTest(TestCase):
     """ Test module for Review model """
 
     def setUp(self):
-        Review.objects.create(stars=5,description="Muito bom",recomends=True,user=User.objects.get(id=1),book=Book.objects.get(id=1))
-        Review.objects.create(stars=1,description="Muito ruim",recomends=False,user=User.objects.get(id=1),book=Book.objects.get(id=1))
+        user = User.objects.create_user(username='test', password='test123')
+        book = Book.objects.create(title="Habitos Atomicos",author="James Cordan",pages=400)
+        Review.objects.create(stars=5,description="Muito bom",recomends=True,user=user,book=book)
+        Review.objects.create(stars=1,description="Muito ruim",recomends=False,user=user,book=book)
 
 
     def test_review_stars(self):
-        review_bom = Review.objects.get(stars=5)
-        review_ruim = Review.objects.get(name=1)
+        review_bom = Review.objects.get(pk=1).stars
+        review_ruim = Review.objects.get(pk=2).stars
         self.assertEqual(
             review_bom, 5)
         self.assertEqual(
