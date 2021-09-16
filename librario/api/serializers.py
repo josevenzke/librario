9ddc592ef6e39e9e21f79d953bc26a67d5252ae5
josevenzke <josevenzkejr@gmail.com>
2021-09-16 00:31:45 -0300

    created serializer for author model

--- librario/api/serializers.py
+++ librario/api/serializers.py
@@ -1,11 +1,15 @@
-from django.db.models import fields
 from rest_framework import serializers
-from .models import Book, Review
+from .models import Book, Review, Author
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'author', 'pages']
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['id','name', 'age']
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
