bde16c43e8f5a67d5ddef50a087a29ba4be02272
josevenzke <josevenzkejr@gmail.com>
2021-09-16 00:32:33 -0300

    added url to new list/add authors views

--- librario/api/urls.py
+++ librario/api/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('list-books/', views.listBooks,name='list-books'),
-    path('add-book/',views.addBook,name='add-book'),
-    path('list-reviews/',views.listReviews,name='list-reviews'),
-    path('list-book-reviews/<int:book_id>/',views.listReviewsFromBook,name='list-book-reviews'),
-    path('add-review/<int:book_id>/',views.addReview,name='add-review')
+    path('list-books/', views.listBooks, name='list-books'),
+    path('add-book/', views.addBook, name='add-book'),
+    path('list-authors/', views.listAuthors, name='list-authors'),
+    path('add-author/', views.addAuthor, name='add-author'),
+    path('list-reviews/', views.listReviews, name='list-reviews'),
+    path('list-book-reviews/<int:book_id>/', views.listReviewsFromBook, name='list-book-reviews'),
+    path('add-review/<int:book_id>/', views.addReview, name='add-review')
 ]
