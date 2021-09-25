4baae9c0d79a9fb4a3a440ac356b44444a3eb613
josevenzke <josevenzkejr@gmail.com>
2021-09-24 21:29:38 -0300

    now using simplejwt django framework

--- librario/librario/settings.py
+++ librario/librario/settings.py
@@ -84,9 +84,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'librario.wsgi.application'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES':(
-        'rest_framework.authentication.TokenAuthentication',
-    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES':(
         'rest_framework.permissions.IsAuthenticated',
     )
