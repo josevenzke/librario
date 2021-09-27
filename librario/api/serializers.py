2a010d5c389838fc8f4a3c531d5a023252cf9fb6
josevenzke <josevenzkejr@gmail.com>
2021-09-26 22:26:27 -0300

    created user serializer

--- librario/api/serializers.py
+++ librario/api/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 from .models import Book, Review, Author, Tag
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
